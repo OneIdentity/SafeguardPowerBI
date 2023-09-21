ebb1cddbeb54dbf0471b45122f44b970c448444f
Marcell Meszaros <marcell.meszaros@oneidentity.com>
2023-09-21 14:26:28 +0200

    tmp

--- SafeguardSessions/Scripts/RunUnitTests.ps1
+++ SafeguardSessions/Scripts/RunUnitTests.ps1
@@ -1,7 +1,4 @@
-Param(
-    $Output,
-    $TestFile
-    )
+Param($TestFile)
 
 $ErrorActionPreference = "Stop"
 
@@ -10,17 +7,78 @@ $dir = Split-Path $scriptpath
 
 . $dir\PreCheck.ps1
 
-if ($null -eq $Output) {
-    $Output = "short"
+$Output = "short"
+if ($null -eq $TestFile){
+    $testInput = $config.unit_tests_path
+} else {
+    $testInput = $TestFile
 }
 
-if ($null -eq $TestFile) {
-    $TestFile = $config.unit_tests_path
-}
-
-& pqtest run-test `
+$pqtestOutput =  pqtest run-test `
     --extension $config.connector_mez_path `
     --extension $config.test_mez_path `
     --environmentConfiguration output=$Output `
-    --queryFile $TestFile `
-    --prettyPrint
+    --queryFile $testInput `
+    --prettyPrint 
+
+$resultsAsObject =  $pqtestOutput | ConvertFrom-Json
+
+function LogError {
+    param (
+        $testResult
+    )
+    Write-Host "$($testResult.Name): " -NoNewline
+    Write-Host "Error" -ForegroundColor red
+    Write-Host "Message: " -NoNewline -ForegroundColor red
+    Write-Host $testResult.Error.Message
+    Write-Host "Details: " -NoNewline -ForegroundColor red
+    Write-Host $testResult.Error.Details
+
+}
+
+function LogSucces {
+    param (
+        $testResult
+    )
+    Write-Host "$($testResult.Name): " -NoNewline
+    Write-Host  "Success" -ForegroundColor green
+}
+
+function LogFail {
+    param (
+        $testResult
+    )
+    Write-Host "$($testResult.Name): " -NoNewline
+    Write-Host "Fail" -ForegroundColor red
+    for (($i = 1); $i -lt $testResult.Output.Details.Length; $i++){
+        $cleanedTestResult = $testResult.Output[$i].Details.Trim()
+        # remove opening and closing parenthesis and split string to expected and actual output
+        $resultArray = $cleanedTestResult.Substring(1, $cleanedTestResult.Length-2) -split "<>" 
+        $expectedResult = $resultArray[0].Trim()
+        $actualResult = $resultArray[1].Trim()
+        Write-Host  "Assert Failed: " -NoNewline -ForegroundColor red
+        Write-Host $testResult.Output[$i].Notes 
+        Write-Host  "Expected Output: " -NoNewline -ForegroundColor green
+        Write-Host $expectedResult
+        Write-Host  "Actual Output: " -NoNewline -ForegroundColor red
+        Write-Host $actualResult
+    }
+    
+}
+
+$isEveryTestSuccessful =  $true
+foreach ($result in $resultsAsObject) {
+    if ($null -eq $result.Output.Result) { 
+        $isEveryTestSuccessful = $false
+        LogError($result)
+    } elseif ($result.Output.Result -eq "Success") { 
+       LogSucces($result)
+    } else {
+        $isEveryTestSuccessful = $false
+        LogFail($result)
+    }
+ }
+
+ if (!$isEveryTestSuccessful){
+    exit 1
+ }
