d9f74b8945c1de6623a02d37a505d3a78a129f9a
Andrew Amburn <andrew.amburn@quest.com>
2023-09-26 15:37:55 -0500

    Added AssetAccount, AssetPartition Entitlement reports; moved login token var to global var; added Constants for the Entitlement reports; renamed reports so that they sort better

--- SafeguardPasswords/build.ps1
+++ SafeguardPasswords/build.ps1
@@ -1,2 +1,25 @@
+
+#Parse and increment Version 
+$ver = get-content .\Version.txt
+$splitVer = $ver.Split(".")
+$intVer = [int]::Parse($splitVer[2]) + 1
+$splitVer[2] = $intVer
+$newVer = [string]::Join(".",$splitVer)
+
+#New file name to include version
+$name = "OneIdentitySafeguardForPrivilegedPasswords-$newVer.mez"
+$path = "C:\$env:HOMEPATH\OneDrive - Quest\Documents\Power BI Desktop\Custom Connectors"
+
+#Update Constants file
+$constantsFile = get-content ..\Common\Modules\Constants.pqm
+$newConstantsFile = $constantsFile.Replace("$ver","$newVer")
+$newConstantsFile | Out-File ..\Common\Modules\Constants.pqm -Encoding utf8
+
+#Update Version file
+$newVer > .\Version.txt
+
+#Make and move
 make
-cp bin/OneIdentitySafeguardForPrivilegedPasswords.mez "C:\Users\aamburn\OneDrive - Quest\Documents\Power BI Desktop\Custom Connectors"
+copy-item bin/OneIdentitySafeguardForPrivilegedPasswords.mez "$path\$name"
+
+# cp bin/OneIdentitySafeguardForPrivilegedPasswords.mez "C:\Users\aamburn\OneDrive - Quest\Documents\Power BI Desktop\Custom Connectors"
