831a63884d2e36750c4bc92e08ae4b563c9df0cc
Szabolcs Szekér <szabolcs.szeker@quest.com>
2024-04-04 15:48:52 +0200

    all: Rework bootstrap
    Scope: /
    
    By moving all build related code to the repository
    root, the CI/CD pipeline can be simplified. Only
    one bootstrap is needed to build and test all projects.
    
    This also makes it possible to automate the tests of
    the common code.

--- Environment/Scripts/Build.ps1
+++ Environment/Scripts/Build.ps1
@@ -1,9 +1,14 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$Project
+)
+
 $ErrorActionPreference = "Stop"
 
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 
-. $dir\PreCheck.ps1
+. $dir\PreCheck.ps1 -Project $Project
 
 & dotnet build $config.ConnectorProjPath -target:Clean,BuildMez -v d --no-restore
 & dotnet build $config.TestProjPath -target:Clean,BuildMez -v d --no-restore
--- Environment/Scripts/LoadConfig.ps1
+++ Environment/Scripts/LoadConfig.ps1
@@ -1,4 +1,9 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$Project
+)
+
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 
-Get-Content -Raw -Path $dir\..\.venv\config.json | ConvertFrom-Json
+Get-Content -Raw -Path $dir\..\..\.venv\$Project.config.json | ConvertFrom-Json
